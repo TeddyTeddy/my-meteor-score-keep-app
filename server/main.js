0a1ef036b1ee6b7644b8a0e38e3a2bf6a2eea73f
HC <hakan.cuzdan@yandex.com>
2018-01-14 15:59:40 +0200

    Section 3: Lecture 16: Adding the first entry to MongoDB and fetching it

--- server/main.js
+++ server/main.js
@@ -4,4 +4,9 @@ import {Meteor} from 'meteor/meteor';
 console.log('Log from /server/main.js');
 Meteor.startup(function() {
 // will run when meteor app is ready
+  Players.insert({
+    name: 'Andrew',
+    score: 3
+  });
+  console.log(Players.find().fetch());
 });
