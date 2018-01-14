77c9b4e8bb5ffba6758e5f366247779a2cf39359
HC <hakan.cuzdan@yandex.com>
2018-01-14 17:51:44 +0200

    Section 3: Lecture 18 : 1.st Challange Done, 2.nd Challange beginning

--- client/main.js
+++ client/main.js
@@ -5,35 +5,8 @@ import {Tracker} from 'meteor/tracker';
 
 import {Players} from './../imports/api/players'
 
-// DELETE THIS
-// autorun monitors the queries executed inside the function
-// and one of the queries changes it re-runs the function
-Tracker.autorun(function () {
-  console.log('Players list', Players.find().fetch()); // sync call
-});
-
-// DELETE THIS
-const players = [{
-  _id:'1',
-  name : 'Lauren',
-  score: 99
-}, {
-  _id:'2',
-  name : 'Cory',
-  score: -1
-}, {
-  _id:'3',
-  name : 'Andrew',
-  score: -11
-}, {
-  _id:'4',
-  name : 'Hakan',
-  score: 8
-}];
-
 // how do we get the players array into JSX?
 const renderPlayers = function(playersList) {
-
   return playersList.map(function (player) {
     return <p key={player._id}>{player.name} has a score {player.score}</p>;
   });
@@ -41,22 +14,21 @@ const renderPlayers = function(playersList) {
 
 // once the dom ready (rendered)
 Meteor.startup(function() {
-  // call Tracker.autorun
-  //   create variable called players -> set equal to fetch query
-  // Render players to the screen
+  // autorun monitors the queries executed inside the function
+  // and one of the queries changes it re-runs the function
+  Tracker.autorun(function () {
+    let players = Players.find().fetch(); // sync call
+    // render some JSX-JavaScript-XML to screen
+    let title = 'Score Keep';
+    let name = 'Andrew';
+    let jsx = (
+      <div>
+        {/*Render h1 tag with title var as text*/}
+        <h1>{title}</h1>
+        {renderPlayers(players)}
+      </div>
+    );
+    ReactDOM.render(jsx, document.getElementById('app'))
+  });
 
-
-  // render some JSX-JavaScript-XML to screen
-  let title = 'Score Keep';
-  let name = 'Andrew';
-  let jsx = (
-    <div>
-      {/*Render h1 tag with title var as text*/}
-      <h1>{title}</h1>
-      <p>Hello {name}!</p>
-      <p>This is my second p.</p>
-      {renderPlayers(players)}
-    </div>
-  );
-  ReactDOM.render(jsx, document.getElementById('app'))
 });
