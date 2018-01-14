61049d7c762f0a9992e0f163995f1ce1b57cd1c7
HC <hakan.cuzdan@yandex.com>
2018-01-14 15:44:01 +0200

    Section 3: Lecture 16: Creating empty files and folders

--- server/main.js
+++ server/main.js
@@ -1,9 +1,1 @@
-//import './../imports/utils';  // executes the file
-import {greetUser, name} from './../imports/utils';
-import add from './../imports/math';
-
 console.log('Log from /server/main.js');
-console.log(greetUser());
-console.log(name);
-
-console.log(add(2,7));
