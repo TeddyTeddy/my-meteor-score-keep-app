8bdc74dbee6331378dd44990a2d1169991a5b374
HC <hakan.cuzdan@yandex.com>
2018-01-18 11:32:19 +0200

    Section 3: Lecture 19 : Challenge1 : Inserting dynamic players done

--- client/main.js
+++ client/main.js
@@ -15,14 +15,18 @@ const renderPlayers = function(playersList) {
 const handleSubmit = function(event) {
   // event is for the form
   // event.target is form
-  let playerName = e.target.playerName.value; // Name value entered by user
+  let playerName = event.target.playerName.value; // Name value entered by user
   event.preventDefault(); // will stop reloading the page
   if(playerName) {
     // clear out the value that is set to the form
     event.target.playerName.value = '';
     // the challange: players insert
     // insert the user entered player value and a score of 0
-    
+    // DELETE THIS
+    Players.insert({
+      name: playerName,
+      score: 0
+    });  // snyc call
   }
 };
 
@@ -48,10 +52,4 @@ Meteor.startup(function() {
     );
     ReactDOM.render(jsx, document.getElementById('app'))
   });
-
-  // DELETE THIS
-  Players.insert({
-    name: 'Superman',
-    score: 14
-  });  // snyc call
 });
