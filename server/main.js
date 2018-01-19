90acb9fa2bd3bce136a6a632f951c64eaa1d3d28
HC <hakan.cuzdan@yandex.com>
2018-01-19 17:00:15 +0200

    Section 3: Lecture 21: Challange Beginning: Converting ES5 functions in client/main.js to Arrow functions

--- server/main.js
+++ server/main.js
@@ -2,11 +2,6 @@ import {Players} from './../imports/api/players'
 import {Meteor} from 'meteor/meteor';
 
 console.log('Log from /server/main.js');
-Meteor.startup(function() {
-// will run when meteor app is ready
-  // Players.insert({
-  //   name: 'Hakan',
-  //   score: 1
-  // });  // snyc call
-  // console.log(Players.find().fetch()); // sync call
+Meteor.startup(() => {
+
 });
