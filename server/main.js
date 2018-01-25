f37bd5997c4fa4e706f72d0b32a44713480fd1e8
HC <hakan.cuzdan@yandex.com>
2018-01-25 19:48:05 +0200

    Section 4: Lecture 26: Person Class and Challange

--- server/main.js
+++ server/main.js
@@ -3,5 +3,14 @@ import {Meteor} from 'meteor/meteor';
 
 console.log('Log from /server/main.js');
 Meteor.startup(() => {
-
+  class Person {
+    constructor(name ='Anonymys') {
+      this.name = name;
+    }
+    getGreeting() {
+      return `Hi! I am ${this.name}`;  // Hi! I am Andrew
+    }
+  }
+  let me = new Person('Mike');
+  console.log(me.getGreeting());
 });
