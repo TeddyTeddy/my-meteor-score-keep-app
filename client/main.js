fc6f1c0f5b5d8acfd0cabd3c763a1ba32e6ea854
HC <hakan.cuzdan@yandex.com>
2018-01-28 17:07:27 +0200

    Section 4: Lecture 35: <App> Component Complete

--- client/main.js
+++ client/main.js
@@ -5,10 +5,6 @@ import {Tracker} from 'meteor/tracker';
 
 import {Players} from './../imports/api/players';
 import App from './../imports/ui/App'
-import TitleBar from './../imports/ui/TitleBar';
-import AddPlayer from './../imports/ui/AddPlayer';
-import PlayerList from './../imports/ui/PlayerList'
-
 
 // once the dom ready (rendered)
 Meteor.startup( () => {
@@ -16,17 +12,7 @@ Meteor.startup( () => {
   // and one of the queries changes it re-runs the function
   Tracker.autorun( () => {
     let players = Players.find().fetch(); // sync call
-    // render some JSX-JavaScript-XML to screen
     let title = 'Score Keep';
-    let subtitle = 'Provided by Hakan Cuzdan';
-    let jsx = (
-      <div>
-        {/*Render h1 tag with title var as text*/}
-        <TitleBar title={title} subtitle={subtitle}/>
-        <PlayerList players={players}/>
-        <AddPlayer/>
-      </div>
-    );
-    ReactDOM.render(<App/>, document.getElementById('app'))
+    ReactDOM.render(<App title={title} players={players}/>, document.getElementById('app'))
   });
 });
--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import TitleBar from './TitleBar';
+import AddPlayer from './AddPlayer';
+import PlayerList from './PlayerList'
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        app Component
+        {/*Render h1 tag with title var as text*/}
+        <TitleBar title={this.props.title} subtitle={this.props.subtitle}/>
+        <PlayerList players={this.props.players}/>
+        <AddPlayer/>
       </div>
     );
   }
 }
+
+App.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
+  players: PropTypes.array.isRequired
+};
