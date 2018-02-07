f27e9a7c81b6955885f1f5e9f5fd0e7102b79696
HC <hakan.cuzdan@yandex.com>
2018-02-07 20:01:06 +0200

    Section 5: Lecture 42: Adding Block Element Modifier to .item class and use it in <PlayerList/> when empty

--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -5,7 +5,9 @@ import Player from './Player'
 export default class PlayerList extends React.Component {
   renderPlayers() {
     if(this.props.players.length === 0) {
-      return (<div className='item'><p>Add your first player to get started</p></div>);
+      return (<div className='item'>
+                <p className='item__message item__message--empty'>Add your first player to get started</p>
+              </div>);
     } else {
       return this.props.players.map( player => {
         return <Player key={player._id} player={player}/>;
