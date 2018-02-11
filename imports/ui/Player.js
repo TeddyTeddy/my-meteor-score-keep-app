06e7bcda6ad7f7b819eecaca034f8a39c70930c5
HC <hakan.cuzdan@yandex.com>
2018-02-11 11:30:02 +0200

    Section 5: Lecture 42: Adding styling to buttons : Lecture end

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -6,9 +6,9 @@ export default class Player extends React.Component {
   render() {
     return <div key={this.props.player._id} className='item'>
       <p>{this.props.player.name} has {this.props.player.score} point(s).</p>
-      <button onClick={() => Players.update(this.props.player._id, {$inc: {score: 1}})}>+1</button>
-      <button onClick={() => Players.update(this.props.player._id, {$inc: {score: -1}})}>-1</button>
-      <button onClick={() => Players.remove(this.props.player._id)}>X</button>
+      <button className='button button--round' onClick={() => Players.update(this.props.player._id, {$inc: {score: 1}})}>+1</button>
+      <button className='button button--round' onClick={() => Players.update(this.props.player._id, {$inc: {score: -1}})}>-1</button>
+      <button className='button button--round' onClick={() => Players.remove(this.props.player._id)}>X</button>
     </div>;
   }
 }
