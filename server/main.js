5f3c534be401978bce7ed7c18382265bab663b67
HC <hakan.cuzdan@yandex.com>
2018-03-04 14:05:12 +0200

    Section 5: Lecture 46: Object Property Shorthand and Object Spread Operator explained

--- server/main.js
+++ server/main.js
@@ -5,3 +5,49 @@ console.log('Log from /server/main.js');
 Meteor.startup(() => {
 
 });
+
+// Object Spread Operator
+let user = {
+  name: 'Andrew',
+  location: 'Helsinki'
+};
+
+let person = {
+  ...user,
+  age: 25
+};
+
+//console.log(person);
+
+// Object Property Shorthand
+let bike = 'Scott';
+let stuff = {
+  bike,
+  laptop: 'Mac'
+};
+
+// console.log(stuff);
+
+let house = {
+  bedrooms: 2,
+  bathrooms: 1.5
+};
+let yearBuilt = 1995;
+
+/*
+{
+ bedrooms: 3
+ bathrooms: 1.5
+ yearBuilt: 1995,
+ flooring: 'Carpet'
+}
+*/
+
+let result = {
+  ...house,
+  bedrooms:3,
+  yearBuilt,
+  flooring: 'Carpet'
+}
+
+console.log(result);
